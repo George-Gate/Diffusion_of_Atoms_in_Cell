da2c8c2b395a8f59bf0ca53c14bb0952bd227a0d
George-Gate <terrorist_1992@hotmail.com>
2017-04-19 14:06:41 +0800

    Add makeMesh3D_cubic

--- programs_cubic_Spectrum_LinearEq/getCoeffs3D_Lobatto.m
+++ programs_cubic_Spectrum_LinearEq/getCoeffs3D_Lobatto.m
@@ -1,7 +1,8 @@
 function [ Mvec, Svec, MPintegralList, QintegralList  ] = getCoeff_Lobatto( boundaryType, mesh )
 % Generate the coeff matrices for a box area.
 %    Do not support non constant first kind boundary condition so far. 
-%    mesh.xList; mesh.yList; mesh.zList; mesh.type='cubic'
+%    mesh.xList; mesh.yList; mesh.zList :   should be col vectors
+%    mesh.type='cubic'
 %    boundaryType: 'first', 'second', 'firstConstant', 'secondZero'
 %
 %
@@ -22,9 +23,26 @@ function [ Mvec, Svec, MPintegralList, QintegralList  ] = getCoeff_Lobatto( boun
 
 
 % read mesh
+if strcmp(boundaryType,'firstConstant')
+    boundaryBasis=0;
+else
+    boundaryBasis=1;
+end
 switch mesh.type
     case 'cubic'
-        
+        Nx=length(mesh.xList);
+        Ny=length(mesh.yList);
+        Nz=length(mesh.zList);
+        if (boundaryBasis)
+            Nnodes=(Nx)*(Ny)*(Nz);
+            Nedges=
+            Nsurfaces=
+        else
+            Nnodes=(Nx-2)*(Ny-2)*(Nz-2);
+            Nedges=
+            Nsurfaces=
+        end
+        Ndomains=(Nx-1)*(Ny-1)*(Nz-1);
         
         
     otherwise
