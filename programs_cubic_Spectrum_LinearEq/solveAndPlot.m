06748cb4bf3ab691325b1f87d8476a99d0177110
George-Gate <terrorist_1992@hotmail.com>
2017-05-06 19:00:57 +0800

    Update main program

--- programs_cubic_Spectrum_LinearEq/solveAndPlot.m
+++ programs_cubic_Spectrum_LinearEq/solveAndPlot.m
@@ -1,102 +1,123 @@
-load DiffusionFEM.mat;  % load matC and matD
-
+disp(pwd);
 %% physical parameters
-alpha=0.5; % 0.5/s
-w=0.02;   % 0.2cm
-P0=1;    % 1/s
-D_ph=0.1; % 0.1 cm^2/s
-dimRho=length(matD);
-
-T0=10;   % evolution time, in unit of seconds
-L=1;     % length of cubic cell, in unit of cm
-
-funP={@(x)exp( -x.*x * (L/2/w)^2 );...
-      @(y)exp( -y.*y * (L/2/w)^2 );...
-      @(z)T0*P0};
-D=4*T0/L/L*D_ph;
-
-% alpha*T0*matD -> getCoeffs3D
+parName='parSet2';
+%run(['codeVerification/',parName]);
+run(parName);
 
 %% simulation parameters
-K=10;
-% meshGrid={[-1;-w;w;1];[-1;-w;w;1];[-1;1]};  % 9 domains
+pars.K=6;
+% meshGrid={[-1;-pars.w;pars.w;1];[-1;-pars.w;pars.w;1];[-1;1]};  % 9 domains
 meshGrid={[-1;1];[-1;1];[-1;1]};  % 1 domain
-% meshGrid={[-1;1];[-1;1];[-1;0;0.5;0.6;0.7;1]};  % 5 domains
+% meshGrid={[-1;1];[-1;1];[-1;0;1]};  % 5 domains
 
-
+dimRho=pars.dimRho;
 %% initial state and boundary condition
-boundaryType='firstUniform';
+pars.rho_0=ones(dimRho,1)/8/dimRho;
+
+boundaryType='secondZero';
 % boundaryType='second';
-rho_0=ones(dimRho,1)/8/dimRho;
-rho_b=ones(dimRho,1)/8/dimRho;
+if strcmp(boundaryType,'firstUniform')
+    pars.rho_b=ones(dimRho,1)/8/dimRho;
+elseif strcmp(boundaryType,'secondZero')
+    % nothing to do
+end
 
 
 %% initialize
 % mesh
-tic;
+fprintf('makeMesh...... ');tic;
 mesh0=makeMesh3D_cubic(meshGrid{1},meshGrid{2},meshGrid{3});
-disp(['Time to makeMesh: ',num2str(toc),' s']);tic;
-% coefficient matrix
-% if strcmp(boundaryType,'firstUniform') || strcmp(boundaryType,'first')
-    [ MM, SS, MG, Nbasis, fun2No, No2fun, getNoByIxyz_a ]=getCoeffs3D('Bilinear+Lobatto',mesh0,K,dimRho,funP,matC,alpha*T0*matD,boundaryType);
-    H=-D*SS-MG;
-% end
-disp(['Time to getCoeffs: ',num2str(toc),' s']);
+disp([num2str(toc),' s']);
 
-tic;
+% coefficient matrix
+fprintf('getCoeffs...... ');tic;
+% if strcmp(boundaryType,'firstUniform') || strcmp(boundaryType,'first')
+    [ M, MM, SS, MG, Nbasis, fun2No, No2fun, getNoByIxyz ]=getCoeffs3D('Bilinear+Lobatto',mesh0,pars.K,dimRho,pars.funP,pars.matC,pars.alpha*pars.T0*pars.matD,boundaryType);
+    H=-pars.D*SS-MG;
+% end
+disp([num2str(toc),' s']);
+
+
 % boundary
+fprintf('Set boundary...... ');tic;
 if strcmp(boundaryType,'firstUniform')
-    if sum(abs(rho_0-rho_b))<eps
-        ngp=K+10;
+    if sum(abs(pars.rho_0-pars.rho_b))<eps
+        ngp=pars.K+10;
         u_0=zeros(Nbasis*dimRho,1);
-        Gf=calcGf( mesh0,funP,rho_b,matC,alpha*T0*matD,getNoByIxyz_a,dimRho,K,Nbasis,ngp );
+        Gf=calcGf( mesh0,pars.funP,pars.rho_b,pars.matC,pars.alpha*pars.T0*pars.matD,getNoByIxyz,dimRho,pars.K,Nbasis,ngp );
     else
         error('Unsupported initial condition.');
     end
+elseif strcmp(boundaryType,'secondZero')
+    Gf=zeros(Nbasis*dimRho,1);
+    % expand the initial state by solving linear system M*u=vec{(v_i,rho_0)}
+    u_0=zeros(Nbasis*dimRho,1);
+    vec1=M\basisInnerProduct( 1, getNoByIxyz, pars.K, Nbasis, mesh0, 3);
+    for i=1:dimRho
+        u_0(1+(i-1)*Nbasis:i*Nbasis)=pars.rho_0(i)*vec1;
+    end
 end
-disp(['Time to Set Boundary: ',num2str(toc),' s']);
-%% time evolution
-opts=odeset('Mass',MM);
-tic;
-[tList,u]=ode45(@(t,u)H*u-Gf,[0,1],u_0,opts);
-disp(['Time to calc evolution: ',num2str(toc),' s']);
+disp([num2str(toc),' s']);
 
-tic;
-filename=['K=',num2str(K),'.mat'];
-save(filename,'alpha','w','P0','D_ph','dimRho','T0','L','funP','K','meshGrid',...
-              'mesh0','boundaryType','rho_0','rho_b','Nbasis','fun2No','No2fun','getNoByIxyz_a',...
-              'tList','u');
-disp(['Time to save result to disk: ',num2str(toc),' s']);
+%% time evolution
+fprintf('Time evolution...... ');tic;
+opts=odeset('Mass',MM,'MaxStep',0.1/pars.T0);
+t_span=linspace(0,1,500*pars.T0+1);
+[soltList,u] = ode45(@(t,u)H*u-Gf,t_span,u_0,opts);
+disp([num2str(toc),' s']);
+
+% shift solution
+if strcmp(boundaryType,'secondZero')
+    u=u-repmat(u_0.',size(u,1),1);
+end
+
+fprintf('Save result to disk...... ');tic;
+filename=[parName,' K=',num2str(pars.K),', T0=',num2str(pars.T0),'  boundary=',boundaryType,'.mat'];
+save(filename,'pars','meshGrid',...
+              'mesh0','boundaryType','Nbasis','fun2No','No2fun','getNoByIxyz',...
+              'soltList','u');
+disp([num2str(toc),' s']);
+
 
 %% mapping to space-time domain
-tic;
-tid=[10000];
-combinedTime=reshape(tList(tid),length(tid),1);
-sol=combineSolution( u(tid,:), '3D', mesh0, getNoByIxyz_a, K, Nbasis, dimRho, 100 );
-disp(['Time to combine solution: ',num2str(toc),' s']);
+dimRho=pars.dimRho;
+fprintf('Combine solution...... ');tic;
+viewDim='1D';
+if strcmp(viewDim,'1D')
+    samplingLines={
+    %               linspace(-1,1,200),zeros(1,200),zeros(1,200),'xAxis';
+    %               zeros(1,200),linspace(-1,1,200),zeros(1,200),'yAxis';
+    %               zeros(1,200),zeros(1,200),linspace(-1,1,200),'zAxis';
+    %               linspace(-1,1,200),zeros(1,200),-0.99*ones(1,200),'xAxis2';
+                   0,0,0,'centerPoint';
+                   0.5,0.5,0.5,'(0.5,0.5,0.5)';
+     };
+    stmax=pars.T0;
+    sNpoints=100;
+    tid=[1:max(1,round(500*stmax/sNpoints)):500*stmax,500*stmax+1];
+    sol=combineSolution( u(tid,:), '1D', mesh0, getNoByIxyz, pars.K, Nbasis, dimRho, samplingLines);
+elseif strcmp(viewDim,'3D')
+    tid=125;
+    sol=combineSolution( u(tid,:), '3D', mesh0, getNoByIxyz, pars.K, Nbasis, dimRho, 50 );
+end
+combinedTime=reshape(soltList(tid),length(tid),1);
+disp([num2str(toc),' s']);
 
 %% plot
-hf=figure('position',[1426 82 1811 533]);
-tid=1;
-for rhoComponent=1:8
-    subplot(2,4,rhoComponent);
+resultPlotter( sol, pars, combinedTime );
 
-    for Did=1:mesh0.Ndomains
-        midPos=sum(mesh0.domains.xyz(:,:,Did),2)/2;
-        domW=mesh0.domains.h(:,Did)/2;
-        xGrid=sol.xSample*domW(1)+midPos(1);
-        yGrid=sol.ySample*domW(2)+midPos(2);
-        zGrid=sol.zSample*domW(3)+midPos(3);
-        hs=slice(xGrid,yGrid,zGrid,sum(sol.rho{Did}(:,:,:,tid,rhoComponent),5),midPos(1),midPos(2),midPos(3));
-        for i=1:length(hs)
-            hs(i).EdgeColor='none';
-        end
+%% get analytical solution
+anaSol_diff.sampleDim='1D';
+for Lid=1:length(sol.tag)
+    px=sol.xList{Lid};py=sol.yList{Lid};pz=sol.zList{Lid};
+    anaSol_diff.tag{Lid}=sol.tag{Lid};
+    anaSol_diff.xList{Lid}=px;
+    anaSol_diff.yList{Lid}=py;
+    anaSol_diff.zList{Lid}=pz;
+    anaSol_diff.rho{Lid}=zeros(1,length(combinedTime),pars.dimRho);
+    for kkk=1:length(combinedTime)
+        anaSol_diff.rho{Lid}(1,kkk,:)=reshape(anaSol(pars.rho_0,combinedTime(kkk),px,py),1,1,pars.dimRho)-sol.rho{Lid}(1,kkk,:);
     end
-    view([26 6]);
-    colormap winter
-    colorbar;
-    title(['\rho_',num2str(rhoComponent)]);
-    xlabel('x');ylabel('y');zlabel('z');
 end
-titleTextBox=uicontrol(hf,'style','text');
-annotation('textbox','String',{['\rho(t)-\rho(0),  K=',num2str(K)],['T=',num2str(T0*combinedTime(tid)),' s']},'Position',[0.02 0.8 0.1 0.1]);
+resultPlotter( anaSol_diff, pars, combinedTime );
+close;
